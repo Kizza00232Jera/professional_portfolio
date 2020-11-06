6c3b1a145310ff325f5f1b77ad73696b93ee505a
Kizza00232Jera <antonio.jera10@gmail.com>
2020-11-06 00:42:29 +0000

    fixed navigation
    when clicking on about me, page is now reloading

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,5 @@
+/* burger menu on and off*/
+
 function menuOn(){
     const nav = document.querySelector('header > nav');
     nav.classList.add('active');
@@ -7,3 +9,13 @@ function menuOff(){
     const nav = document.querySelector('header > nav');
     nav.classList.remove('active');
 }
+
+/* refresh page on mobiles when clicking on about me or projects*/ 
+const refreshSkills = document.querySelector('.refresh')
+
+const refreshPage = () => {
+    location.reload();
+    // console.log('here');
+}
+
+refreshSkills.addEventListener('click', refreshPage);
